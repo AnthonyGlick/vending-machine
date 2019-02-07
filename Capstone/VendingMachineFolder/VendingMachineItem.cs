b12bf2b8e2a35b83c2abad0916b07319e63d0887
Peter Scanniello <pscanniello216@gmail.com>
2019-02-07 14:00:50 -0500

    added more to capstone

--- Capstone/VendingMachineFolder/VendingMachineItem.cs
+++ Capstone/VendingMachineFolder/VendingMachineItem.cs
@@ -6,11 +6,28 @@ namespace Capstone.VendingMachineFolder
 {
     public abstract class VendingMachineItem
     {
-     public string Slot { get; }
-     public string Name { get; }
-     public decimal Price { get; }
-     public string Type { get; }
-     public int RemainingInventory { get; private set; }
+        public string Slot { get; }
+        public string Name { get; }
+        public decimal Price { get; }
+        public string Type { get; }
+        public int RemainingInventory { get; private set; }
+        public string Message { get; private set; }
+
+        public VendingMachineItem(string slot, string name, decimal price, string type, int remainingInventory)
+        {
+            this.Slot = slot;
+            this.Name = name;
+            this.Price = price;
+            this.Type = type;
+            this.RemainingInventory = remainingInventory;
+        }
+
+
+        
+
+
+    
+     
 
    
     }
